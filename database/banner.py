a71a7d2f99944a5015a013bb2c29cdf8e72350d1
MidD3Vil <79802337+MidD3Vil@users.noreply.github.com>
2021-12-01 12:32:55 -0300

    Update banner.py

--- database/banner.py
+++ database/banner.py
@@ -37,15 +37,17 @@ def menu():
 ┗  ┏━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓  ┛
    ┃    {Nyellow}[{Dgreen} CONSULTAS {Nyellow}]{Ired}    ┃   {Nyellow}[{Iblue} FERRAMENTAS {Nyellow}]{Ired}   ┃   {Nyellow}[{Twhite} OPÇÕES {Nyellow}]{Ired}   ┃
    ┃                     ┃                     ┃                ┃
-   ┣┫{Nyellow}[01]{Dgreen} Consulta CEP{Ired}   ┣┫{Nyellow}[06]{Iblue} Meu IP{Ired}         ┣┫{Nyellow}[09]{Twhite} Atualizar{Ired} ┃
+   ┣┫{Nyellow}[01]{Dgreen} Consulta Nome{Ired}  ┣┫{Nyellow}[07]{Iblue} Meu IP{Ired}         ┣┫{Nyellow}[10]{Twhite} Atualizar{Ired} ┃
    ┃                     ┃                     ┃                ┃
-   ┣┫{Nyellow}[02]{Dgreen} Consulta CPF{Ired}   ┣┫{Nyellow}[07]{Iblue} Covid Info{Ired}     ┣┫{Nyellow}[10]{Twhite} Sair{Ired}      ┃
+   ┣┫{Nyellow}[02]{Dgreen} Consulta CPF{Ired}   ┣┫{Nyellow}[08]{Iblue} Covid Info{Ired}     ┣┫{Nyellow}[11]{Twhite} Sair{Ired}      ┃
    ┃                     ┃                     ┃                ┃
-   ┣┫{Nyellow}[03]{Dgreen} Consulta IP{Ired}    ┣┫{Nyellow}[08]{Iblue} Root Checker{Ired}   ┣┫{Nyellow}[11]{Twhite} Criador{Ired}   ┃
+   ┣┫{Nyellow}[03]{Dgreen} Consulta CEP{Ired}   ┣┫{Nyellow}[09]{Iblue} Root Checker{Ired}   ┣┫{Nyellow}[12]{Twhite} Criador{Ired}   ┃
    ┃                     ┃                     ┃                ┃
-   ┣┫{Nyellow}[04]{Dgreen} Consulta Placa{Ired} ┣┫                    ┣┫{Nyellow}[12]{Twhite} Grupo{Ired}     ┃
+   ┣┫{Nyellow}[04]{Dgreen} Consulta Placa{Ired} ┣┫                    ┣┫{Nyellow}[13]{Twhite} Grupo{Ired}     ┃
    ┃                     ┃                     ┃                ┃
    ┣┫{Nyellow}[05]{Dgreen} Consulta CNPJ{Ired}  ┣┫                    ┣┫               ┃
+   ┃                     ┃                     ┃                ┃
+   ┣┫{Nyellow}[06]{Dgreen} Consulta IP{Ired}    ┣┫                    ┣┫               ┃
    ┗━━━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━┛+
 
 ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛''')
